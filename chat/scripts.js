6f00304faa1744e3d34ab482288488a1454d3b4d
Dzmitry Niadzelka <week62@gmail.com>
2015-03-22 00:46:41 +0200

    Add delete | edit, rename

--- chat/scripts.js
+++ chat/scripts.js
@@ -1,12 +1,23 @@
-var defValueTextArea = document.getElementById('newMessage').defaultValue;
-function send (event) {	
+var run = function () {
+	var sendButton = document.getElementById('sendButton');
+	var newMessage = document.getElementById('newMessage');
+	var buttonEditProfile = document.getElementById('buttonEditProfile');
+	sendButton.onclick = send;
+	buttonEditProfile.onclick = showEditProfile;
+	newMessage.addEventListener("keydown", enterTextArea);
+}();
+
+function send (event) {
+	var currentUserName = document.getElementById('currentUserName'); 	
 	var newMessage = document.getElementById('newMessage'); 
 	var chatField = document.getElementById('chatField');
 	var scrollDown = document.getElementById('scrollDown');
-	newMessage.value = newMessage.value.replace(/\r?\n/g, '<br>');     							
+	newMessage.value = newMessage.value.replace(/\r?\n/g, '<br>');    							
 	chatField.innerHTML += '<li class="media"><div class="media-body"><div class="media"><a class="pull-left" href="#"><img class="media-object img-circle" src="user.png"></a><div class="media-body">'+
 	newMessage.value
-	+'<br><small class="text-muted">Jhon Rexa | 23rd June at 5:00pm</small><small class="text-muted pull-right"><a href="#">Edit</a> | <a href="#">Delete</a></small><hr></div></div></div></li>';
+	+'<br><small class="text-muted"><span class="userNameEditDelete">'+
+	currentUserName.textContent
+	+'</span> | 23rd June at 5:00pm</small><small class="text-muted pull-right editDelete"><a href="#">Edit</a> | <a href="#">Delete</a></small><hr></div></div></div></li>';
 	newMessage.value = '';
 	//scrollDown.scrollTop = scrollDown.scrollHeight;
 }
@@ -28,17 +39,19 @@ function showEditProfile (event) {
 }
 
 function submitEditedProfile (event) {
+	currentUserName = document.getElementById('currentUserName');
 	inputEditProfile = document.getElementById('inputEditProfile');
 	formEditProfile = document.getElementById('formEditProfile');
-	previousUserName.innerHTML = inputEditProfile.value;
+	currentUserName.innerHTML = inputEditProfile.value;
 	formEditProfile.style.display = 'none';
-}
 
-var events = function () {
-	var sendButton = document.getElementById('sendButton');
-	var newMessage = document.getElementById('newMessage');
-	var buttonEditProfile = document.getElementById('buttonEditProfile');
-	sendButton.onclick = send;
-	buttonEditProfile.onclick = showEditProfile;
-	newMessage.addEventListener("keydown", enterTextArea);
-}();
+	var messagesArray = document.getElementsByClassName('text-muted pull-right editDelete');
+	var usersArray = document.getElementsByClassName('userNameEditDelete');
+	for (var i = 0; i < usersArray.length; ++i) {
+		if (currentUserName.textContent !== usersArray[i].textContent) {
+			messagesArray[i].innerHTML = '';
+		} else if (currentUserName.textContent === usersArray[i].textContent){
+			messagesArray[i].innerHTML = '<a href="#">Edit</a> | <a href="#">Delete</a>';
+		}
+	}
+}
