b7412670ad86f38edb0daf841b5042acb37f907e
Dzmitry Niadzelka <week62@gmail.com>
2015-03-22 15:19:45 +0200

    edit and delete functions is working!

--- chat/scripts.js
+++ chat/scripts.js
@@ -1,38 +1,30 @@
 var run = function () {
 	var sendButton = document.getElementById('sendButton');
-	var newMessage = document.getElementById('newMessage');
+	var newMessageTextArea = document.getElementById('newMessageTextArea');
 	var buttonEditProfile = document.getElementById('buttonEditProfile');
-	sendButton.onclick = send;
 	buttonEditProfile.onclick = showEditProfile;
-	newMessage.addEventListener("keydown", enterTextArea);
+	sendButton.onclick = send;
 }();
 
 function send (event) {
+	//will not send form (reload page), if click submit button
+	if (event.preventDefault) {
+    	event.preventDefault();
+   	}
 	var currentUserName = document.getElementById('currentUserName'); 	
-	var newMessage = document.getElementById('newMessage'); 
+	var newMessageTextArea = document.getElementById('newMessageTextArea'); 
 	var chatField = document.getElementById('chatField');
-	var scrollDown = document.getElementById('scrollDown');
-	newMessage.value = newMessage.value.replace(/\r?\n/g, '<br>');    							
-	chatField.innerHTML += '<li class="media"><div class="media-body"><div class="media"><a class="pull-left" href="#"><img class="media-object img-circle" src="user.png"></a><div class="media-body">'+
-	newMessage.value
-	+'<br><small class="text-muted"><span class="userNameEditDelete">'+
+	newMessageTextArea.value = newMessageTextArea.value.replace(/\r?\n/g, '<br>');    							
+	chatField.innerHTML += '<li class="media"><div class="media-body"><div class="media"><a class="pull-left" href="#"><img class="media-object img-circle" src="user.png"></a><div class="media-body edit"><span class="currentChatText">'+
+	newMessageTextArea.value
+	+'</span><br><small class="text-muted"><span class="userNameEditDelete">'+
 	currentUserName.textContent
 	+'</span> | '+
 	getTime()
 	+'</small><small class="text-muted pull-right editDelete"><a href="#">Edit</a> | <a href="#">Delete</a></small><hr></div></div></div></li>';
 	scanDeleteMessage ();
 
-	newMessage.value = '';
-	//scrollDown.scrollTop = scrollDown.scrollHeight;
-}
-
-function enterTextArea (event) {
-	var newMessage = document.getElementById('newMessage');
-	if (event.keyCode === 13 && event.ctrlKey) {
-		newMessage.value += '\r\n';
-	} else if (event.keyCode === 13) {
-		send();
-	}
+	newMessageTextArea.value = '';
 }
 
 function showEditProfile (event) {
@@ -43,7 +35,7 @@ function showEditProfile (event) {
 }
 
 function submitEditedProfile (event) {
-	//will not send form (reload page), if cleck submit button
+	//will not send form (reload page), if click submit button
 	if (event.preventDefault) {
     event.preventDefault();
    	} 
@@ -71,17 +63,51 @@ function scanDeleteMessage () {
 		if (editeDeleteArray[i].lastChild !== null) {
 			editeDeleteArray[i].lastChild.onclick = deleteMessage;
 		}
+		if(editeDeleteArray[i].firstChild !== null) {
+			editeDeleteArray[i].firstChild.onclick = editeMessage;
+		}
 	}
 }
 
 function deleteMessage (event) {
 	var deleteLi = event.target;
-	console.log(deleteLi);
 	while (deleteLi.tagName != 'LI') {
-		console.log(deleteLi.tagName);
 		deleteLi = deleteLi.parentNode;
 	}
 	deleteLi.innerHTML = '<div class="row"><div class="col-md-12 text-center"><small class="text-muted center">Message was deleted</small></div></div>';	
+}
+
+function editeMessage (event) {
+	var editLi = event.target;
+	while (editLi.className != 'media-body edit') {
+		editLi = editLi.parentNode;
+	}
+	for (var i = 0; i < editLi.firstChild.childNodes.length; ++i) {
+		if (editLi.firstChild.childNodes[i].tagName === 'BR') {
+			editLi.firstChild.childNodes[i].innerHTML = '\n';
+		}
+	}
+	var currentTextAreaContent = editLi.firstChild.textContent;  
+	editLi.innerHTML = '<div class="input-group"><textarea type="text" class="form-control" id="editMessageTextArea" style="height:60px; resize: none">'+
+	currentTextAreaContent 
+	+'</textarea><span class="input-group-btn"><button type="submit" class="btn btn-info pull-right" id="editButtonTextArea" style="height:60px">edit</button></span></div>';
+	var editMessageTextArea = document.getElementById('editMessageTextArea');
+	var editButtonTextArea = document.getElementById('editButtonTextArea');
+	editButtonTextArea.onclick = function (event) {
+			//will not send form (reload page), if click submit button
+			if (event.preventDefault) {
+    			event.preventDefault();
+   			}
+   			editMessageTextArea.value = editMessageTextArea.value.replace(/\r?\n/g, '<br>');
+   			editLi.innerHTML = '<span class="currentChatText">'+
+			editMessageTextArea.value
+			+'</span><br><small class="text-muted"><span class="userNameEditDelete">'+
+			currentUserName.textContent
+			+'</span> | '+
+			getTime()
+			+'</small><small class="text-muted pull-right editDelete"><a href="#">Edit</a> | <a href="#">Delete</a></small><hr>';
+			scanDeleteMessage (); 
+	}
 }
 
 function getTime () {
